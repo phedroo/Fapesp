406df7776df6c173485d519a18c180c396e1d319
phedroo <pedro.marucio@gmail.com>
2025-03-23 10:41:25 -0300

    prodes

--- R/prodes-manipulacao.R
+++ R/prodes-manipulacao.R
@@ -3,25 +3,28 @@
 # Carregando biblioteca
 library(raster) # Ler dados GeoTIFF
 library(geobr)
+library(dplyr)
 library(sf)
 library(terra)
 
-# Carregando basse de dados Raster
+# Carregando base de dados Raster
 dados <- raster("data-raw/desmatamento_prodes/prodes_brasil_2023.tif")
+
+dados2 <- rast("data-raw/desmatamento_prodes/prodes_brasil_2023.tif") # Verificando
 # class(dados)
 
 # Verificando metadados
-plot(dados)
+plot(dados2)
 
 # Limites dos estados de interesse
 estados <- read_state(year = 2020, code_state = "all", showProgress = FALSE) |> 
   filter(abbrev_state %in% c("MT", "MS", "GO"))
 
 # Converter para o mesmo sistema de coordenadas do raster
-estados <- st_transform(estados, crs(dados))
+estados <- st_transform(estados, crs(dados2))
 
 # Recorte do raster para a região de interesse
-recorte <- crop(dados, estados)
+recorte <- crop(dados2, estados)
 
 # Visualizar
 plot(recorte)
