d58241c57de407ae4547e346c7fb8525aedcbfea
phedroo <pedro.marucio@gmail.com>
2025-04-21 10:23:42 -0300

    Tratamento dos dados appeears (.csv)

--- R/ler-appeears.R
+++ R/ler-appeears.R
@@ -1,14 +1,5 @@
 # Ler arquivos .csv do appeears
 
-# *os arquivos foram requisitados em 4 etapas:
-# Após extraidas as coordenadas do Climate TRACE (cerca de 1150),
-# foram solicitadas as primeiras
-# 999 (requisição do appeears não aceita mais que 1000),
-# 1. Solicitadas 999 para ET
-# 2. Solicitadas 999 para Fpar e Lai
-# 3. Solicitadas 999 para EVI e NDVI (único arquivo csv)
-# 4. Solicitadas as coordenadas restantes para todas as camadas anteriores (único arquivo csv)
-
 # Carregar biblioteca
 library(tidyverse)
 
